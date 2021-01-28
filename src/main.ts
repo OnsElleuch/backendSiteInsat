0934197dfb19c45d7c841562d1f2efa24308acca
Samielleuch <45316444+Samielleuch@users.noreply.github.com>
2021-01-28 17:34:30 +0100

    added Swager Docs Generation (#7)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: ['http://localhost:4200/'],
   });
+  const options = new DocumentBuilder()
+    .setTitle('Backend Insat')
+    .setDescription('site Insat Frontedn and Backoffice REST API Backend')
+    .setVersion('1.0')
+    .addTag('insat')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
