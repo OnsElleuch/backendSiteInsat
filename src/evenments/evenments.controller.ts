cdb3c30e4437724f75f92894e74259498f43ce68
Ons Elleuch <Ons.Elleuch@insat.u-carthage.tn>
2021-01-30 12:58:00 +0100

    Revert "Revert "merging main into develop ""

---
+++ src/evenments/evenments.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { EvenmentsService } from './evenments.service';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+
+@ApiTags('evenments')
+@Controller('evenments')
+export class EvenmentsController {
+  constructor(private evenmentsService: EvenmentsService) {}
+
+  @Get('getEvents')
+  getNews() {
+    return this.evenmentsService.getEvents();
+  }
+
+  @Get('getEventsById/:id')
+  getEventsById(@Param('id') id: number) {
+    return this.evenmentsService.getEventById(id);
+  }
+
+  @Post('createEvent/')
+  createEvent(@Body() createEventDto: CreateEventDto) {
+    return this.evenmentsService.createEvent(createEventDto);
+  }
+
+  @Put('updateEvent/:id')
+  updateEvent(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
+    return this.evenmentsService.updateEvent(+id, updateEventDto);
+  }
+
+  @Delete('removeEvent/:id')
+  removeEvent(@Param('id') id: string) {
+    return this.evenmentsService.removeEvent(+id);
+  }
+}
---
+++ src/evenments/evenments.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { EventEntity } from './entities/event.entity';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+
+@Injectable()
+export class EvenmentsService {
+  constructor(
+    @InjectRepository(EventEntity)
+    private readonly EventsRepository: Repository<EventEntity>,
+  ) {}
+  async getEvents(): Promise<EventEntity[]> {
+    return await this.EventsRepository.find();
+  }
+  async getEventById(id: number) {
+    return await this.EventsRepository.find({ id: id });
+  }
+  async updateEvent(id: number, updateEventDto: UpdateEventDto) {
+    return await this.EventsRepository.update({ id: id }, updateEventDto);
+  }
+
+  async removeEvent(id: number) {
+    return await this.EventsRepository.delete(id);
+  }
+  async createEvent(createEventDto: CreateEventDto) {
+    return await this.EventsRepository.save(createEventDto);
+  }
+}
