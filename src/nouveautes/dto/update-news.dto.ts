89869da96a22286c9ebce27308436ce118ec30c5
Ons Elleuch <Ons.Elleuch@insat.u-carthage.tn>
2021-01-30 12:57:09 +0100

    Revert "merging main into develop "

--- src/nouveautes/dto/update-news.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNewsDto } from './create-news.dto';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UpdateNewsDto extends PartialType(CreateNewsDto) {
-  @ApiProperty()
-  id: number;
-}
