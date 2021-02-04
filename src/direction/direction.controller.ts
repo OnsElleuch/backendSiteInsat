4150b7bc01abdbab5361b842b147f8045e33183c
unknown <ons.elleuch.oe@gmail.com>
2021-02-04 12:47:56 +0100

    added pagination and delete

--- src/direction/direction.controller.ts
+++ src/direction/direction.controller.ts
@@ -1,8 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { DirectionService } from './direction.service';
 import { DirectionEntity } from './entities/direction.entity';
 import { Direction } from './models/direction';
+import {
+  paginate,
+  Pagination,
+  IPaginationOptions,
+} from 'nestjs-typeorm-paginate';
 @ApiTags('direction')
 @Controller('direction')
 export class DirectionController {
@@ -10,16 +24,34 @@ export class DirectionController {
 
   @Get('getDirectionMembers')
   getDirectionMembers(): Promise<DirectionEntity[]> {
-    return this.directionService.getDirectionMembers();
+    return this.directionService.getDirectionMembersByService('Direction');
   }
 
   @Get('getAdministrationMembers')
   getAdministrationMembers(): Promise<DirectionEntity[]> {
-    return this.directionService.getAdministrationMembers();
+    return this.directionService.getDirectionMembersByService('Administration');
+  }
+
+  @Get('getAllMembers')
+  getAllMembers(
+    @Query('page', ParseIntPipe) page: number = 1,
+    @Query('limit', ParseIntPipe) limit: number = 10,
+  ): Promise<Pagination<DirectionEntity>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.directionService.getAllMembers({
+      page,
+      limit,
+      route: 'http://localhost:3000/direction/getAllMembers',
+    });
   }
 
   @Post('addMember')
   addMember(@Body() member: Direction) {
     return this.directionService.addMember(member);
+  }
+
+  @Delete('deleteMemberById/:poste_code')
+  deleMemberById(@Param() poste_code: number) {
+    return this.directionService.deleteMemberById(poste_code);
   }
 }
